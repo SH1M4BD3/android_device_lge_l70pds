45dc6fa0e205542a08bbba8600c4b2b5c224970c
Hakam El Assouad <hakamelassouad@gmail.com>
2015-03-03 16:43:54 +0100

    Rebase ramdisk path

--- device_l70pds.mk
+++ device_l70pds.mk
@@ -1,5 +1,9 @@
 LOCAL_PATH := device/lge/l70pds
 
+# Ramdisk
+PRODUCT_COPY_FILES += \
+	   $(LOCAL_PATH)/ramdisk/recovery.fstab:root/recovery.fstab \
+
 $(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
@@ -10,7 +14,6 @@ endif
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/kernel:kernel \
-    $(LOCAL_PATH)/dt.img:dt.img \
-    $(LOCAL_PATH)/recovery.fstab:root/recovery.fstab \
-
+    $(LOCAL_PATH)/dt.img:dt.img 
+    
 $(call inherit-product, build/target/product/full.mk)
